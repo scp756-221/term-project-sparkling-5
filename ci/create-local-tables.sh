40a6e00a92ce779c11c1dbe25db18b8070e53829
Yutao Huang <lhhyt1994@gmail.com>
2022-04-11 18:29:40 -0700

    add s1 and s2 config back

--- ci/create-local-tables.sh
+++ ci/create-local-tables.sh
@@ -7,20 +7,20 @@ set -o xtrace
 # are required by the CLI
 # but ignored for this local version of DynamoDB.
 
-# aws dynamodb create-table \
-#   --endpoint-url http://0.0.0.0:8000 \
-#   --region us-west-2 \
-#   --table-name Music-kridergod \
-#   --attribute-definitions '[{ "AttributeName": "music_id", "AttributeType": "S" }]' \
-#   --key-schema '[{ "AttributeName": "music_id", "KeyType": "HASH" }]' \
-#   --provisioned-throughput '{"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}'
-# aws dynamodb create-table \
-#   --endpoint-url http://0.0.0.0:8000 \
-#   --region us-west-2 \
-#   --table-name User-kridergod \
-#   --attribute-definitions '[{ "AttributeName": "user_id", "AttributeType": "S" }]' \
-#   --key-schema '[{ "AttributeName": "user_id", "KeyType": "HASH" }]' \
-#   --provisioned-throughput '{"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}'
+aws dynamodb create-table \
+  --endpoint-url http://0.0.0.0:8000 \
+  --region us-west-2 \
+  --table-name Music-kridergod \
+  --attribute-definitions '[{ "AttributeName": "music_id", "AttributeType": "S" }]' \
+  --key-schema '[{ "AttributeName": "music_id", "KeyType": "HASH" }]' \
+  --provisioned-throughput '{"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}'
+aws dynamodb create-table \
+  --endpoint-url http://0.0.0.0:8000 \
+  --region us-west-2 \
+  --table-name User-kridergod \
+  --attribute-definitions '[{ "AttributeName": "user_id", "AttributeType": "S" }]' \
+  --key-schema '[{ "AttributeName": "user_id", "KeyType": "HASH" }]' \
+  --provisioned-throughput '{"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}'
 aws dynamodb create-table \
   --endpoint-url http://0.0.0.0:8000 \
   --region us-west-2 \
